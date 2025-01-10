6055232ab98b1b79d06eb082ff682272c7862c27
Mohammad Atout <atout.m@live.unic.ac.cy>
2025-01-10 11:10:00 +0100

    Comments Two

--- MenuPosition.cs
+++ MenuPosition.cs
@@ -15,7 +15,6 @@ public class MenuPosition
     public string Name { get; set; } // Name of the menu item
 
     // Optional Attribute
-    // make it otional
     public string Description { get; set; }
     public string Allergens { get; set; } // Optional information about allergens (e.g., "Contains nuts")
 
